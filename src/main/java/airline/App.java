79b99541d2b44f056c9b57cfdd0f42dd01a7587a
David Guevara <37723586+DatWaft@users.noreply.github.com>
2019-10-15 20:41:07 -0600

    windows classic look

--- src/main/java/airline/App.java
+++ src/main/java/airline/App.java
@@ -1,6 +1,8 @@
 package airline;
 
 import airline.presentation.mainwindow.*;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class App
 {
@@ -10,6 +12,15 @@ public class App
   
   public static void main(String[] args)
   {
+    try
+    {
+      UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
+    } 
+    catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) 
+    {
+      System.err.println("error: invalid look and feel.");
+    }
+    
     model = new Model();
     view = new View();
     controller = new Controller(model, view);
